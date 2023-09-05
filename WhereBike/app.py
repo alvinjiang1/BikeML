05ad420b0d47a3f335eddcd7f378db0437949216
Alvin  Jiang <alvinjiang1@gmail.com>
2023-09-05 10:24:03 +0800

    Implement ML Prediction
    WhereBike users can now make use of the input fields to create predictions, using a trained, 'no lag variables' version of the Random Forest Regressor

--- WhereBike/app.py
+++ WhereBike/app.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for
 from joblib import load
 import pandas as pd
-from WhereBike.helper import user_generate_df
+from helper import user_generate_df
 import helper
 
 app = Flask(__name__)
 
 # Load the trained model
 model = load('rf.pkl')
+# Load the No lag model:
+no_lag_model = load('no_lag_rf.pkl')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -28,6 +30,7 @@ def submit():
         return predict(result_map)
     
 def predict(args):
+    print(args)
     # Make predictions based on the data that was input   
 
     # Temperature: Normalize the temperature: (t-t_min)/(t_max-t_min), t_min=-8, t_max=+39 
@@ -36,25 +39,37 @@ def predict(args):
     # Time: Getting the hour from the time
     args["time"] = int(args["time"][:2])
 
-    # Weather: Obtain the numerical value for the weather
-    args["weather"] = int(args["weather"][7])
+    # Weather: Obtain the numerical value for the weather    
+    if args["weather"] == '':
+        args["weather"] = helper.MODE_WEATHER
+    else:
+        args["weather"] = int(args["weather"][7])
 
     # Humidity: Normalize the temperature, The values are divided to 100 (max)
-    args["humidity"] = int(args["humidity"]) / 100
+    if args["humidity"] == '':
+        args["humidity"] = helper.MEAN_HUM
+    else:
+        args["humidity"] = int(args["humidity"]) / 100
 
     # Windspeed: Normalize the windspeed, The values are divided to 67 (max)
-    args["windspeed"] = int(args["windspeed"]) / 67
+    if args["windspeed"] == '':
+        args["windspeed"] = helper.MEAN_WINDSPEED
+    else:
+        args["windspeed"] = int(args["windspeed"]) / 67
 
-    # Perform pre-processing on input to allow it to match    
-    processed_df = user_generate_df(args)
-
+    result = None
+    isUser = True
     # Use model to perform predictions
-    result = model.predict(processed_df)
+    if isUser:
+        # Preprocess input
+        processed_df = user_generate_df(args)
+        result = no_lag_model.predict(processed_df)
+    else:
+        result = model.predict(processed_df)
+    assert result != None
+    predicted_demand = round(result[0])
+    return jsonify(prediction=predicted_demand)
 
-    return jsonify(prediction=12345)
-
-def preprocess(df):
-    raise NotImplementedError
     
 @app.route('/vendor.html')
 def vendor():
--- WhereBike/helper.py
+++ WhereBike/helper.py
@@ -2,8 +2,11 @@ import pandas as pd
 from datetime import datetime
 
 # Humidity: Mean = 0.627229
+MEAN_HUM = 0.727229
 # Windspeed: Mean = 0.190098
+MEAN_WINDSPEED = 0.190098
 # Weather: Mode = 1 (Clear)
+MODE_WEATHER = 1
 
 
 features = ['yr',
@@ -64,7 +67,7 @@ no_lag_features = ['yr',
  'weekday_5',
  'weekday_6']
 
-a = {'temperature': '1', 'time': '23:47', 'weather': 'weather2', 'humidity': '2', 'windspeed': '0', 'date': '2023-09-05'}
+a = {'temperature': '1', 'time': '23:47', 'weather': None, 'humidity': None, 'windspeed': None, 'date': '2023-12-05'}
 def user_generate_df(df):
     result = pd.DataFrame(index = [0], columns = no_lag_features)
     
@@ -83,14 +86,35 @@ def user_generate_df(df):
         else:
             result['weekday_' + str(i + 1)] = 0
     # Month
-
+    month = date_obj.month
+    for i in range(2, 13):
+        if month == i:
+            result["mnth_" + str(i)] = 1
+        else:
+            result["mnth_" + str(i)] = 0
     # Year
+    result["yr"] = date_obj.year
 
-    # Working Day
+    # Working Day: Imputed with whether the day is a weekday
+    if day < 5:
+        result["workingday"] = 1
+    else:
+        result["workingday"] = 0
 
-    # Holiday
+    # Holiday: Imputed with max_freq: 0
+    result['holiday'] = 0
 
-    # Humidity
+    # Humidity: Imputed with mean humidity
+    result['hum'] = df['humidity']
 
-    # Windspeed
-user_generate_df(a)
+    # Windspeed: Imputed with mean windspeed
+    result["windspeed"] = df["windspeed"]
+    
+    # Weather: Imputed with mode weather      
+    weather = df["weather"]    
+    for i in range(2, 5):
+        if i == weather:
+            result["weathersit_" + str(i)] = 1
+        else:
+            result["weathersit_" + str(i)] = 0    
+    return result
